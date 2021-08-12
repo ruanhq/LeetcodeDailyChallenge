a8024f00e4414290f7bcc12a79bec3a42db8ec3d
Heqiao Ruan <hruan@ucdavis.edu>
2021-08-12 10:02:24 -0700

    Update 740.py

--- 740.py
+++ 740.py
@@ -1,1 +1,12 @@
 #740. DeleteAndEarn:
+#use dp and do the mapping of nums
+from collections import Counter
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        counters = Counter(nums)
+        freqMap = [0] * (max(nums) + 1)
+        for c in counters:
+            freqMap[c] = c * counters[c]
+        for i in range(2, max(nums) + 1):
+            freqMap[i] = max(freqMap[i - 2] + freqMap[i], freqMap[i - 1])
+        return freqMap[-1]
