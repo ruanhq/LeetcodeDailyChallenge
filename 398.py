93bd191c6c7d2f71b0d9352ad6bb34ef2abc7062
Heqiao Ruan <hruan@ucdavis.edu>
2021-08-14 22:09:24 -0700

    Update 398.py

--- 398.py
+++ 398.py
@@ -4,6 +4,8 @@ import random
 #you can assume that the given target number must exist in 
 #the array.
 
+#It likes the reservoir sampling where each time 
+#using a 1/#currentNumber probability
 
 class Solution:
     def __init__(self, nums: List[int]):
@@ -15,6 +17,19 @@ class Solution:
         for i, x in enumerate(self.nums):
             if x == target:
                 countTarget += 1
-            if random.randint(1, count) == countTarget:
-                result = i 
+                #like reservoir sampling:
+                #each time sample with 1/currentNumber probability here
+                #if K distinct values:
+                #P(t) = (1/t) * (t/(t+1)) * ..(k-1/k) = 1/k
+                if random.randint(1, count) == countTarget:
+                    result = i 
         return result
+
+    #Another quicker method, just do one random choice here.
+    def pick(self, target):
+        targetResult = []
+        for i, x in enumerate(self.nums):
+            if x == target:
+                targetResult.append(i)
+        output = random.choice(targetResult)
+        return output
