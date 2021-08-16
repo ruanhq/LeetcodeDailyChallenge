d2d146a1512ea33ec3e4b3eb357e2ba4608d686b
Heqiao Ruan <hruan@ucdavis.edu>
2021-08-15 22:21:46 -0700

    Update 528.py

--- 528.py
+++ 528.py
@@ -18,3 +18,32 @@ class Solution:
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
+
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        result = 0
+        heap = []
+        intervals.sort(key = lambda X: X[0])
+        for interval in intervals:
+        	#If there is no conflict, remove the smallest right interval point.
+            while heap and heap[0] <= interval[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, interval[1])
+            result = max(result, len(heap))
+        return result
+
+
+#######
+
+def minMeeting
+
+
+
+
+
+
+
+
+
+
