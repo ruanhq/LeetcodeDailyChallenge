7da592ed5be6dc14eaabc66679ef4dd9ccc881e3
Heqiao Ruan <hruan@ucdavis.edu>
2021-08-20 14:08:37 -0700

    Update 137.py

--- 137.py
+++ 137.py
@@ -1,40 +1,10 @@
 #137. Missing Number:
 class Solution:
     def singleNumber(nums):
-        if len(nums) == 1:
-            return nums
-        result = 0
-        for i in range(32):
-        	sums = 0
-        	for num in nums:
-        	    sums += (nums >> i) & 1
-        	    sums %= 3
-        	result = result | (sums << i)
-        return res
-    ########
-    ########
-    ########
-    ########
-    def singleNumber(nums):
-        if len(nums) == 1:
-            return nums
-        result = 0 
-        for i in range(32):
-            sums = 0 
-            for num in nums:
-                sums += (nums >> i) & 1
-                sums %= 3
-            result = result | (sums << i)
+        assumedSum = 3 * sum(set(nums))
+        result = (sum(nums) - assumedSum) // 2
         return result
-
-    def singleNumber(nums):
-        if len(nums) == 1:
-            return nums[0]
-        result = 0
-        for i in range(32):
-            sums = 0
-            for num in nums:
-                sums += (num >> i) & 1
-                sums %= 3
-            result = result | (sums << i)
-        return result
+    ########
+    ########
+    ########
+    ########
