0dfa45da0e5b0fcafa5b5f2a2b220653aa389e13
Heqiao Ruan <hruan@ucdavis.edu>
2021-08-20 14:05:05 -0700

    update

--- 222.py
+++ 222.py
@@ -6,13 +6,13 @@ class Solution:
 	    return self.depthCount(root.left) + 1
 	#Complete binary tree: the higher layer should be complete and the last layer is as left as possible
     def countNodes(self, root):
-    	if not root:
-    	    return 0
-    	leftDepth = self.depthCount(root.left)
-    	rightDepth = self.depthCount(root.right)
-    	## left tree is complete, right tree can be complete
-    	if leftDepth == rightDepth:
-    	    return 2 ** leftDepth + self.countNodes(root.right)
-    	## right tree is complete here, left tree can be not complete
-    	else:
-    	    return 2 ** rightDepth + self.countNodes(root.left)
+	    if not root:
+	        return 0
+	    leftDepth = self.depthCount(root.left)
+	    rightDepth = self.depthCount(root.right)
+	    ## left tree is complete, right tree can be complete
+	    if leftDepth == rightDepth:
+	        return 2 ** leftDepth + self.countNodes(root.right)
+	    ## right tree is complete here, left tree can be not complete
+	    else:
+	        return 2 ** rightDepth + self.countNodes(root.left)
--- 324.py
+++ 324.py
@@ -24,18 +24,6 @@ class Solution:
             return 0 
         return self.getDepth(root.left)
 
-class Solution:
-    def countNodes(self, root):
-        if not root:
-            return 0
-        leftDepth = self.getDepth(root.left)
-        rightDepth = self.getDepth(root.right)
-        if leftDepth == rightDepth:
-            return pow(2, leftDepth) + self.countNodes(root.right)
-        else:
-            return pow(2, rightDepth) + self.countNodes(root.left)
-
-
 
 
 
