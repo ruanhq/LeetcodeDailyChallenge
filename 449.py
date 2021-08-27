b5c4c8cf87900f7939e76a5a20988117d3d7cff4
rhq <ruanhq1994@gmail.com>
2021-08-26 18:06:29 -0700

    Update 449.py

--- 449.py
+++ 449.py
@@ -1,1 +1,32 @@
+from collections import deque
+class Codec:
+    def serialize(self, root):
+        if not root:
+            return ''
+        result = []
+        queues = deque([root])
+        while queues:
+            currentNode = queues.popleft()
+            if currentNode:
+                result.append(str(currentNode.val))
+                queues.extend([currentNode.left])
+                queues.extend([currentNode.right])
+            else:
+                result.append("-")
+        return ",".join(result)
+    def deserialize(self, data):
+        if not data:
+            return None
+        treeList = deque(data.split(','))
+        rootNode = TreeNode(int(treeList.popleft()))
+        queues = deque([rootNode])
+        while queues:
+            currentnode = queues.popleft()
+            if (leftOne := treeList.popleft()) != "-":
+                currentnode.left = TreeNode(int(leftOne))
+                queues.append(currentnode.left)
+            if (rightOne := treeList.popleft()) != "-":
+                currentnode.right = TreeNode(int(rightOne))
+                queues.append(currentnode.right)
+        return rootNode
 
